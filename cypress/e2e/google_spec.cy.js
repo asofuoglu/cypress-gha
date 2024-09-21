1fdb4acaef34d9116b5fc8a6abdb3ab629a50cd8
asofuoglu <a.sofuoglu1@gmail.com>
2024-09-21 11:51:56 +0300

    new commit

--- cypress/e2e/google_spec.cy.js
+++ cypress/e2e/google_spec.cy.js
@@ -2,5 +2,6 @@ describe('Google Search', () => {
   it('should open Google', () => {
     cy.visit('https://www.google.com');
     cy.title().should('include', 'Google');
+    cy.title().should('include', 'Google');
   });
 });
