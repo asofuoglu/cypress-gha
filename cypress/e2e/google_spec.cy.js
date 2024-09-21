bb34577e98da1b14cd918a195a9c7d671852ed2b
asofuoglu <a.sofuoglu1@gmail.com>
2024-09-21 12:09:55 +0300

    updated

--- cypress/e2e/google_spec.cy.js
+++ cypress/e2e/google_spec.cy.js
@@ -1,11 +1,9 @@
-describe('Google Search Test', () => {
-  it('searches for a name and clicks on the link with specific text', () => {
-    for (let i = 0; i < 50; i++) {
-    cy.visit('https://www.google.com');
-    cy.get('textarea[name="q"]').type('Abdullatif sofuoğlu');
-    cy.get('textarea[name="q"]').type('{enter}');
-    cy.url().should('include', 'search');
-    cy.contains('Facebook').click(); 
-    }
+describe("Google Search Test", () => {
+  it("searches for a name and clicks on the link with specific text", () => {
+    cy.visit("https://www.google.com");
+    cy.get('textarea[name="q"]').type("Abdullatif sofuoğlu");
+    cy.get('textarea[name="q"]').type("{enter}");
+    cy.url().should("include", "search");
+    cy.contains("Facebook").click();
   });
 });
