70105bad9c5eccb2e15ae55d5082218fed84fc0d
asofuoglu <a.sofuoglu1@gmail.com>
2024-09-21 11:53:36 +0300

    refactored

--- cypress/e2e/google_spec.cy.js
+++ cypress/e2e/google_spec.cy.js
@@ -2,6 +2,5 @@ describe('Google Search', () => {
   it('should open Google', () => {
     cy.visit('https://www.google.com');
     cy.title().should('include', 'Google');
-    cy.title().should('include', 'Google');
   });
 });
