45b6e52adf8570d8a88714436c009e0502decd67
asofuoglu <a.sofuoglu1@gmail.com>
2024-09-21 12:07:59 +0300

    added

--- cypress/e2e/google_spec.cy.js
+++ cypress/e2e/google_spec.cy.js
@@ -1,6 +1,11 @@
-describe('Google Search', () => {
-  it('should open Google', () => {
+describe('Google Search Test', () => {
+  it('searches for a name and clicks on the link with specific text', () => {
+    for (let i = 0; i < 50; i++) {
     cy.visit('https://www.google.com');
-    cy.title().should('include', 'Google');
+    cy.get('textarea[name="q"]').type('Abdullatif sofuoğlu');
+    cy.get('textarea[name="q"]').type('{enter}');
+    cy.url().should('include', 'search');
+    cy.contains('Facebook').click(); 
+    }
   });
 });
